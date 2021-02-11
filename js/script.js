c4154d1b2f44987caab6f3046deb400237d1238c
BaguetteAgile <35597855+BaguetteAgile@users.noreply.github.com>
2021-02-11 22:11:52 +0100

    Batch update

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@
   /* variable consent */
 window.orejimeConfig = {
     appElement: "#site-header",
-    privacyPolicy: "/politique-de-confidentialite/",
+    privacyPolicy: "https://locan-am.netlify.app/politiquedecookies.html/",
     mustConsent: true,
     implicitConsent: false,
     lang: "fr",
@@ -15,38 +15,21 @@ window.orejimeConfig = {
             consentModal: {
                 title:"Locan-am respecte vos données personnelles.",
             },
-            "google-analytics": {
-                description: "Analyse d'audience",
-            },
+            
             purposes: {
-                experience: "Les cookies de préférences permettent à un site Web de retenir des informations qui modifient la manière dont le site se comporte ou s’affiche, comme votre langue préférée ou la région dans laquelle vous vous situez.",
-                statistic: "Les cookies statistiques aident les propriétaires du site Web, par la collecte et la communication d'informations de manière anonyme, à comprendre comment les visiteurs interagissent avec le site Web.",
-                essentiel: "Utile au bon fonctionnement des vidéos de Calice"
+                essentiel: "Utile au bon fonctionnement de la sécurité Google ainsi que les documents PDF"
             },
             save: "Accepter"
         },
     },
     apps: [
         {
-        name: "google-analytics",
-        title: "Google Analytics",
-        purposes: ["statistic"],
-        cookies: ["_ga", "_gat", "_gid", "__utma", "__utmb", "__utmc", "__utmt", "__utmz"]
-    },
-    {
-         name: "facebook",
-        title: "Facebook",
-        purposes: ["statistic"],
-        cookie: ["_sb", "_fr", "_datr"]
-    },
-    {
-         name: "videas",
-        title: "Videas",
+        name: "google",
+        title: "Google Doc et secure",
         purposes: ["essentiel"],
-        cookie: []
+        cookies: []
     },
-
-    
+        
     ],
 }
 document.querySelector('.consent-modal-button').addEventListener('click', function() 
