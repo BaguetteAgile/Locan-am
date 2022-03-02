c9e7bec7063b7ab2f57a94e361b58907647bfc98
BaguetteAgile <35597855+BaguetteAgile@users.noreply.github.com>
2022-03-02 20:14:08 +0100

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1096},"pages":[{"id":"page-home","displayName":"home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-politiquedecookies","displayName":"politiquedecookies","link":{"linkType":"LinkTypePage","href":"#!page-politiquedecookies"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-cgu","displayName":"CGU","link":{"linkType":"LinkTypePage","href":"#!page-cgu"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false}]}
+    window.silex.data = {"site":{"width":1149.0625},"pages":[{"id":"page-home","displayName":"home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-politiquedecookies","displayName":"politiquedecookies","link":{"linkType":"LinkTypePage","href":"#!page-politiquedecookies"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-cgu","displayName":"CGU","link":{"linkType":"LinkTypePage","href":"#!page-cgu"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false}]}
 /*------------------ OREJIME - GPDR ------------------------------------------*/
   /* variable consent */
 window.orejimeConfig = {
