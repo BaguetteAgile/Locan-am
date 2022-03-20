14ec3341fa479555a31721438a81ec574f3e4582
BaguetteAgile <35597855+BaguetteAgile@users.noreply.github.com>
2022-03-20 16:00:11 +0100

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1149.0625},"pages":[{"id":"page-home","displayName":"home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-politiquedecookies","displayName":"politiquedecookies","link":{"linkType":"LinkTypePage","href":"#!page-politiquedecookies"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-cgu","displayName":"CGU","link":{"linkType":"LinkTypePage","href":"#!page-cgu"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false}]}
+    window.silex.data = {"site":{"width":960},"pages":[{"id":"page-home","displayName":"home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-politiquedecookies","displayName":"politiquedecookies","link":{"linkType":"LinkTypePage","href":"#!page-politiquedecookies"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-cgu","displayName":"CGU","link":{"linkType":"LinkTypePage","href":"#!page-cgu"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false}]}
 /*------------------ OREJIME - GPDR ------------------------------------------*/
   /* variable consent */
 window.orejimeConfig = {
@@ -36,3 +36,23 @@ window.orejimeConfig = {
 
             
 /* End variable consent */
+
+/* Jarallax */
+
+$(function() {
+    var elements = $('.jarallax').jarallax({
+        //onInit: function(){ console.log('onInit', this)},
+        onCoverImage: function(){
+            var container = $('#jarallax-container-' + this.instanceID).children()[0]
+            if (container) {
+                container = $(container)
+                container.css({
+                    'width': (container.width() / silex.scale) + 'px',
+                    'height': (container.height() / silex.scale) + 'px',
+                })
+            }
+        }
+    });
+});
+
+/* END Jarallax */
